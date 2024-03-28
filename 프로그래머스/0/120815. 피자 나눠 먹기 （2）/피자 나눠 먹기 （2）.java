e8438259669e26cdd81f2d33c4fa1b5f7e1dbbd2
Ajin <aaajin1220@gmail.com>
2024-03-28 17:41:03 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int solution(int n) {
+		int answer = 0;
+		int max = 0;
+		for (int i = 1; i <= n && i <= 6; i++) {
+			if (n % i == 0 && 6 % i == 0) {
+				max = i;
+			}
+		}
+		return answer = n * 6 / max / 6;
+	}
+}
